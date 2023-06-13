cab2d37ebafe75fb55f88c4255b9db6177db925a
Fakhriddin <fakhmirakov1998@gmail.com>
2023-06-13 10:02:44 +0500

    fixed bugs

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -3,18 +3,30 @@ let logBtn = document.querySelector('.log-in__btn')
 let inputs = document.querySelectorAll('input')
 let form = document.forms.form
 
-// let localData = JSON.stringify(localStorage.getItem('title-object'))
+let patterns = {
+  name: /^[a-z ,.'-]+$/i,
+  surname: /^[a-z ,.'-]+$/i,
+  email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+  password: /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/
+}
 
 form.onsubmit = (e) => {
   e.preventDefault()
 }
 
 inputs.forEach(input => {
+  input.onkeyup = () => {
+    if (patterns[input.name].test(input.value)) {
+      input.style.border = '1px solid green'
+    } else {
+      input.style.border = '1px solid red'
+    }
+  }
+
   signBtn.onclick = () => {
     let data = {}
 
     let fm = new FormData(form)
-    console.log(fm);
 
     fm.forEach((key, value) => {
       data[value] = key
